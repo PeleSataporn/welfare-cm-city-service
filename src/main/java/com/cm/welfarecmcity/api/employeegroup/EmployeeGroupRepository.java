ff646ec39a5806dba66609e62cd12432930bb101
sataporn37 <srujirut@gmail.com>
2023-03-18 14:18:06 +0700

    Fix

--- src/main/java/com/cm/welfarecmcity/api/employeegroup/EmployeeGroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cm.welfarecmcity.api.employeegroup;
-
-import com.cm.welfarecmcity.dto.EmployeeGroupDto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface EmployeeGroupRepository extends JpaRepository<EmployeeGroupDto, Long>, JpaSpecificationExecutor<EmployeeGroupDto> {}
