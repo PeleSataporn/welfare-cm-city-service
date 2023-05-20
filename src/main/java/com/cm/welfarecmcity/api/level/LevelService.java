81957d3a880d6c475c54506a7d3362f029c42b35
sataporn37 <srujirut@gmail.com>
2023-05-20 15:16:21 +0700

    fix

--- src/main/java/com/cm/welfarecmcity/api/level/LevelService.java
+++ src/main/java/com/cm/welfarecmcity/api/level/LevelService.java
@@ -1,10 +1,10 @@
 package com.cm.welfarecmcity.api.level;
 
 import com.cm.welfarecmcity.dto.LevelDto;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class LevelService {
@@ -12,6 +12,7 @@ public class LevelService {
   @Autowired
   private LevelRepository levelRepository;
 
+  @Transactional
   public List<LevelDto> searchLevel() {
     return levelRepository.findAll();
   }
