e60e232bef1fc4ff3eccd4a5807214aeba60c296
sataporn37 <srujirut@gmail.com>
2023-05-31 16:35:22 +0700

    up

--- src/main/java/com/cm/welfarecmcity/WelfareCmCityApplication.java
+++ src/main/java/com/cm/welfarecmcity/WelfareCmCityApplication.java
@@ -4,18 +4,32 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableScheduling
+//@EnableScheduling
+//extends SpringBootServletInitializer
 @SpringBootApplication
-public class WelfareCmCityApplication extends SpringBootServletInitializer {
+public class WelfareCmCityApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(WelfareCmCityApplication.class, args);
   }
 
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    return application.sources(WelfareCmCityApplication.class);
+  //  @Override
+  //  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+  //    return application.sources(WelfareCmCityApplication.class);
+  //  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://203.159.93.121:8080/");
+      }
+    };
   }
 }
