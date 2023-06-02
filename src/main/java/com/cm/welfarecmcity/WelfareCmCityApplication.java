5600e6ecca087dc1061fc8b73bb2da1d52aca055
sataporn37 <srujirut@gmail.com>
2023-06-02 23:22:38 +0700

    up

--- src/main/java/com/cm/welfarecmcity/WelfareCmCityApplication.java
+++ src/main/java/com/cm/welfarecmcity/WelfareCmCityApplication.java
@@ -2,15 +2,7 @@ package com.cm.welfarecmcity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@EnableScheduling
-//extends SpringBootServletInitializer
 @SpringBootApplication
 public class WelfareCmCityApplication {
 
@@ -18,18 +10,4 @@ public class WelfareCmCityApplication {
     SpringApplication.run(WelfareCmCityApplication.class, args);
   }
 
-  //  @Override
-  //  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-  //    return application.sources(WelfareCmCityApplication.class);
-  //  }
-
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://203.159.93.121:8080/");
-      }
-    };
-  }
 }
--- src/main/java/com/cm/welfarecmcity/logic/login/LoginController.java
+++ src/main/java/com/cm/welfarecmcity/logic/login/LoginController.java
@@ -4,12 +4,8 @@ import com.cm.welfarecmcity.dto.ForgetPasswordDto;
 import com.cm.welfarecmcity.dto.UserDto;
 import com.cm.welfarecmcity.dto.base.ResponseData;
 import com.cm.welfarecmcity.dto.base.ResponseModel;
-import com.cm.welfarecmcity.logic.login.model.LoginRes;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/logic/v1/login")
@@ -18,7 +14,13 @@ public class LoginController {
   @Autowired
   private LoginService loginService;
 
-  @PostMapping("/user-login")
+  //  @PostMapping("/user-login")
+  //  @POST
+  //  @Path("/user-login")
+  //  @Produces(MediaType.APPLICATION_JSON)
+  //  @Consumes(MediaType.APPLICATION_JSON)
+
+  @RequestMapping(value = "/user-login", produces = "application/json", method = RequestMethod.POST, consumes = "application/json")
   public ResponseModel<Object> login(@RequestBody UserDto dto) {
     return loginService.login(dto);
   }
