be963f4e526b43f456387a115def5bbd73355613
sataporn37 <srujirut@gmail.com>
2023-06-03 10:43:30 +0700

    up

---
+++ src/main/java/com/cm/welfarecmcity/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.cm.welfarecmcity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+      .addMapping("/**")
+      .allowedOrigins("http://localhost:5000") // Replace with your frontend domain
+      .allowedMethods("GET", "POST", "PUT", "DELETE")
+      .allowedHeaders("*")
+      .allowCredentials(true);
+  }
+}
--- src/main/java/com/cm/welfarecmcity/logic/login/LoginController.java
+++ src/main/java/com/cm/welfarecmcity/logic/login/LoginController.java
@@ -4,7 +4,9 @@ import com.cm.welfarecmcity.dto.ForgetPasswordDto;
 import com.cm.welfarecmcity.dto.UserDto;
 import com.cm.welfarecmcity.dto.base.ResponseData;
 import com.cm.welfarecmcity.dto.base.ResponseModel;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,9 +22,14 @@ public class LoginController {
   //  @Produces(MediaType.APPLICATION_JSON)
   //  @Consumes(MediaType.APPLICATION_JSON)
 
-  @RequestMapping(value = "/user-login", produces = "application/json", method = RequestMethod.POST, consumes = "application/json")
-  public ResponseModel<Object> login(@RequestBody UserDto dto) {
-    return loginService.login(dto);
+  //  @RequestMapping(value = "/user-login", produces = "application/json", method = RequestMethod.POST, consumes = "application/json")
+  @PostMapping("/user-login")
+//  @CrossOrigin("http://localhost:5000")
+  public ResponseEntity<ResponseModel<Object>> login(@RequestBody UserDto dto) {
+    //    val ss = loginService.login(dto);
+    //    return ResponseEntity.ok().build(ss);
+    val response = loginService.login(dto);
+    return ResponseEntity.ok(response);
   }
 
   @PostMapping("/change/forget-password")
