4988d56820c75fade05c52b8d9687bdb7c250ab5
Fluk <satapornsanla36@gmail.com>
2024-03-09 00:54:28 +0700

    update add sumary table to cron job service auto

--- src/main/java/com/cm/welfarecmcity/WelfareCmCityApplication.java
+++ src/main/java/com/cm/welfarecmcity/WelfareCmCityApplication.java
@@ -2,8 +2,10 @@ package com.cm.welfarecmcity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WelfareCmCityApplication {
 
   public static void main(String[] args) {
