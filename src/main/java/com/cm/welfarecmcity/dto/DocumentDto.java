2b8cf386948b680b44a5bf09437de06877adb920
dinopele <srujirut@gmail.com>
2024-09-19 14:52:51 +0700

    Feat (add document entity)

---
+++ src/main/java/com/cm/welfarecmcity/dto/DocumentDto.java
@@ -0,0 +1,22 @@
+package com.cm.welfarecmcity.dto;
+
+import com.cm.welfarecmcity.dto.base.BaseDto;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Document")
+public class DocumentDto extends BaseDto {
+
+    private String name;
+
+    @Lob
+    @Column(name = "pdf_file")
+    private byte[] pdfFile;
+}
