b85b64f031ce47a1d2dd8e7b4c009e6c8556ed95
dinopele <srujirut@gmail.com>
2025-03-02 16:02:49 +0700

    up

--- src/main/java/com/cm/welfarecmcity/api/loandetailhistory/LoanDetailHistoryService.java
+++ src/main/java/com/cm/welfarecmcity/api/loandetailhistory/LoanDetailHistoryService.java
@@ -183,9 +183,12 @@ public class LoanDetailHistoryService {
   @Transactional
   public List<DocumentV2ResLoanV2> searchV2LoanHistory(String getMonthCurrent, String yearCurrent) {
     // Step 1: Fetch loan data
-    val result = new ArrayList<DocumentV2ResLoan>();
+    val result = new ArrayList<DocumentV1ResLoan>();
+//    var resLoan =
+//        loanDetailHistoryLogicRepository.searchV2LoanHistory(getMonthCurrent, yearCurrent);
+
     var resLoan =
-        loanDetailHistoryLogicRepository.searchV2LoanHistory(getMonthCurrent, yearCurrent);
+            loanDetailHistoryLogicRepository.searchV1LoanHistory(getMonthCurrent, yearCurrent);
 
     // Step 2: Filter and collect the valid results
     resLoan.forEach(
@@ -206,7 +209,7 @@ public class LoanDetailHistoryService {
         result.stream()
             .collect(
                 Collectors.groupingBy(
-                    DocumentV2ResLoan::getDepartmentName,
+                        DocumentV1ResLoan::getDepartmentName,
                     Collectors.summingDouble(
                         res ->
                             Double.parseDouble(
